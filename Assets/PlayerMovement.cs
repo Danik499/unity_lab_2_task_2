acdcdeb9f2100b6f0be53296b0e14b534a822966
Danik499 <danikshcherban499@gmail.com>
2020-09-29 10:29:10 +0300

    unity_lab_2_task_2

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+        rb.AddForce(new Vector3(-2, -1, 0) * 20f, ForceMode.Impulse);
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(new Vector3(-1, 0, 0) * 5f * Time.deltaTime);   
+        
+    }
+}
